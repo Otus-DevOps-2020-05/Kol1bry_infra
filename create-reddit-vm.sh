eb6cf641f4e37c7720ef6ea69492fabcd0653469
Kol1bry <Kol1bryph@gmail.com>
2020-09-09 11:54:50 +0300

    Revert "Revert "Revert "packer-base"""
    This reverts commit d3fe370e511a89a11e26da13a4920dd32a6b3cd9.

--- create-reddit-vm.sh
+++ create-reddit-vm.sh
@@ -2,7 +2,7 @@ yc compute instance create \
   --name from-image \
   --hostname from-image \
   --memory=4 \
-  --create-boot-disk image-id='fd8k7271n31fa567vf8r',size=10GB \
+  --create-boot-disk image-id='fhmc3bk5hso1dcmullg4',size=10GB \
   --network-interface subnet-name=default-ru-central1-a,nat-ip-version=ipv4 \
   --metadata serial-port-enable=1 \
   --ssh-key /home/kolibry/.ssh/appuser.pub \
